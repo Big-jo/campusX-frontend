e6b39d012828bbb76a63c65d041c783bf6160f89
Antigha Henshaw <Ghost@localhost.localdomain>
2019-09-18 18:05:51 +0100

    Add user post properties

--- src/app/interfaces/Post.ts
+++ src/app/interfaces/Post.ts
@@ -1,9 +1,21 @@
 export interface IPost {
-  avatar: string;
-  name: string;
-  userTag: string;
+
   post: string;
   likes: number;
   dislikes: number;
   trash: number;
+  author: {
+    name: string,
+    userProfile: {
+    avatar: string,
+    gender: string,
+    level: number,
+    rep_points: number,
+    university: string,
+    userTag: string;
+  };
+};
+  createdAt: string;
+  likedBy: string[];
+  _id: string;
 }
