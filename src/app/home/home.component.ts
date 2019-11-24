865baab4fc145f989c427a1b7a5ffde40abb0f9f
Joseph Henshaw <furiousjoe16@gmail.com>
2019-11-24 17:03:00 +0100

    Feature: Add, pwa add to home screen modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild, AfterViewInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { PostService } from '../services/post.service';
 import { IPost } from '../interfaces/Post';
+import {ModalService} from 'ng-zorro-antd-mobile';
+import {PwaService} from '../services/pwa.service';
+import {NgxSmartModalService} from 'ngx-smart-modal';
+import {NotificationService} from '../services/notification.service';
 
 interface Response {
   posts: Array<IPost>;
@@ -12,58 +16,43 @@ interface Response {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  state = {
-    refreshState: {
-      currentState: 'deactivate',
-      drag: false
-    },
-    direction: '',
-    endReachedRefresh: false,
-    height: 500,
-    data: [],
-    directionName: 'both up and down'
-  };
-  dtPullToRefreshStyle = { height: this.state.height + 'px' };
-  message = 'Pull down to refresh';
-  items = [];
-  Posts: Array<IPost>;
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor(private router: Router, private postService: PostService) {
-   }
+  Posts: Array<IPost>;
+  @ViewChild(TemplateRef, {static: false}) installModal: TemplateRef<any>;
+
+  constructor(private router: Router, private postService: PostService,
+              private ngxSmartModalService: NgxSmartModalService,
+              private pwaService: PwaService,
+              private notificationService: NotificationService
+              ) {
+
+  }
 
   ngOnInit() {
-    console.log(this.Posts);
-
     this.GetPost();
-    console.log(this.Posts);
-
   }
 
-  pullToRefresh(event) {
-    if (event === 'endReachedRefresh') {
-      if (null) {
-
-        this.state.refreshState.currentState = 'release';
-        setTimeout(() => {
-          this.state.refreshState.currentState = 'finish';
-        }, 1000);
-      }
-    } else {
-      if (event === 'down') {
-        this.state.data = [];
-
-      } else {
-        if (null) {
-
-        }
-      }
+  ngAfterViewInit() {
+    console.log(this.pwaService.promptEvent);
+    if (this.pwaService.promptEvent) {
+      this.showAlert();
     }
+    this.notificationService.requestPermission();
   }
-
   GetPost() {
     this.postService.GetPost(1, localStorage.getItem('userID')).subscribe((res: Response) => {
       this.Posts = res.posts;
     });
+  }
+
+  showAlert() {
+    this.ngxSmartModalService.getModal('installModal').open();
+  }
+
+  installPwa(): void {
+    if (this.pwaService.promptEvent.prompt()) {
+      this.showAlert();
+    }
   }
 }
